ea6243926c90f95fe80c00acf67261a3e5c55122
Ruben Salazar <rubensalazar@rocketmail.com>
2025-12-04 13:56:34 -0600

    add new chore form

--- server/Resources/chore.py
+++ server/Resources/chore.py
@@ -1,13 +1,18 @@
 from flask_restful import Resource
-from flask import Response
-import json
-from models import Chore
+from flask import request
+from models import db, Chore
 
 class ChoreList(Resource):
     def get(self):
         chores = Chore.query.all()
-        chores_dict = [chore.to_dict() for chore in chores]
-        
-        pretty_json = json.dumps(chores_dict, indent=2)
-        return Response(pretty_json, mimetype='application/json')  # no , 200 here
+        return [chore.to_dict() for chore in chores], 200
 
+    def post(self):
+        data = request.get_json()
+        if not data.get("title"):
+            return {"error": "Title is required"}, 400
+
+        chore = Chore(title=data["title"], description=data.get("description"))
+        db.session.add(chore)
+        db.session.commit()
+        return chore.to_dict(), 201
